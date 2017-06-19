c391903b9d4ff2d45f2656580b70f2bf866ebc29
j-ew-s <gabriel.scavassa@outlook.com>
2017-06-19 16:48:49 -0300

    Observable with array

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/map'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/common', 'rxjs/Rx', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/map'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounce
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1;
+    var core_1, common_1, Rx_1;
     var AppComponent;
     return {
         setters:[
@@ -19,6 +19,9 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounce
             },
             function (common_1_1) {
                 common_1 = common_1_1;
+            },
+            function (Rx_1_1) {
+                Rx_1 = Rx_1_1;
             },
             function (_1) {},
             function (_2) {}],
@@ -31,6 +34,15 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounce
                     var search = this.form.find('search');
                     search.valueChanges.debounceTime(400)
                         .map(function (str) { return str.replace(' ', '-'); })
+                        .subscribe(function (x) { return console.log(x); });
+                    var dates = [new Date(2017, 7, 9),
+                        new Date(2017, 7, 10),
+                        new Date(2017, 7, 11)];
+                    Rx_1.Observable.fromArray(dates)
+                        .map(function (date) {
+                        console.log("Getting start : " + dates);
+                        return [1, 2, 3];
+                    })
                         .subscribe(function (x) { return console.log(x); });
                 }
                 AppComponent = __decorate([
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,42 @@
-import {Component} from 'angular2/core';
-import {ControlGroup, FormBuilder} from 'angular2/common'
+import { Component } from 'angular2/core';
+import { ControlGroup, FormBuilder } from 'angular2/common'
 
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
+
 
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html',
 })
-export class AppComponent { 
-    form : ControlGroup;
-    
-    
-    constructor(fb: FormBuilder){
-       
+export class AppComponent {
+    form: ControlGroup;
+
+
+
+    constructor(fb: FormBuilder) {
+
         this.form = fb.group({
-                                search : []
-                            });
-        
+            search: []
+        });
+
         var search = this.form.find('search');
         search.valueChanges.debounceTime(400)
-                            .map(str => (<string> str).replace(' ', '-'))
-                            .subscribe( x => console.log(x));
+                            .map(str => (<string>str).replace(' ', '-'))
+                            .subscribe(x => console.log(x));
+
+        var dates = [new Date(2017, 7, 9),
+                    new Date(2017, 7, 10),
+                    new Date(2017, 7, 11)];
+
+        Observable.fromArray(dates)
+                    .map(date => {
+                        console.log("Getting start : " + dates);
+                        return [1, 2, 3];
+                    })
+                    .subscribe(x => console.log(x));
+
+
     }
-
-
 }
